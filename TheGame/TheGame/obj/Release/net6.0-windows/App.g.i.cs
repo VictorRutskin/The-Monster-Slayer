d1bab5eb2591f4ff3ffdbfe7604ea1928a81cefc
Victor Rutskin <Lashaka250375@gmail.com>
2023-01-12 15:34:28 +0200

    Run Here added

--- TheGame/TheGame/obj/Release/net6.0-windows/App.g.i.cs
+++ TheGame/TheGame/obj/Release/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace TheGame {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace TheGame {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             TheGame.App app = new TheGame.App();
             app.InitializeComponent();
